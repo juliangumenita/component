6a4ec10b3984cf8b9ffee6c94e56e3f1ed200d9e
Julian Gumenita <julian@gumenita.com>
2019-02-18 20:18:22 +0300

    Update example file

--- index.php
+++ index.php
@@ -1,14 +1,23 @@
 <?php
   require "Component.php";
+
   echo Component::get("text", [
     "value" => "This is the default component."
   ]);
+  /* Basic usage. */
+
   echo Component::get("text", [
     "value" => "You can use other states as well."
   ], "bold");
+  /* Using states. */
+
   echo Component::get("text", [
     "target" => "_blank",
     "link" => "http://google.com",
     "value" => "You can specify the directory too."
-  ], "link");
+  ], [
+    "state" => "link",
+    "dir" => "component/"
+  ]);
+  /* Complex structure. */
 ?>
